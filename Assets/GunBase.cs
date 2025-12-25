874ff29e1d1b1cf75170aad4aad136fdf0a67b68
ohara <ohara@g.neec.ac.jp>
2025-12-25 16:53:14 +0900

    発射　銃完了

---
+++ Assets/GunBase.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public abstract class GunBase : MonoBehaviour
+{
+    [SerializeField]
+    // 発砲する弾のプレハブ
+    protected RayBullet bulletPrefab_;
+    [SerializeField]
+    // 銃口のトランスフォーム。発射座標
+    protected Transform muzzleTransform_;
+    [SerializeField]
+    // 連射間隔
+    protected float fireRate_ = 0;
+    // 発射タイマー
+    protected float shotTimer_ = 0;
+    [SerializeField]
+    // 威力
+    protected float power_ = 1;
+    [SerializeField]
+    // 取得前に回転している速度
+    private float itemRotateSpeedDeg_ = 90f;
+
+    // 前ページから
+
+    // 発射入力があったら実行。
+    public abstract void OnTrigger();
+    // 発射入力がなかったら実行。
+    public abstract void OffTrigger();
+
+
+    private void ItemRotate()
+    {
+        // 地面に落ちている間の回転処理
+        transform.RotateAround(
+          transform.position,
+          Vector3.up,
+          itemRotateSpeedDeg_ * Time.deltaTime
+        );
+    }
+    // 次ページへ
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 前ページから
+    public bool GetIsAlone()
+    {
+        // 親がいなかったら誰にも所持されていない
+        return transform.parent == null;
+    }
+    public virtual void Update()
+    {
+        // 所持されていなかったら回転する
+        if (GetIsAlone()) { ItemRotate(); }
+        // タイマーの更新
+        if (shotTimer_ <= 0) { return; }
+        shotTimer_ -= Time.deltaTime;
+    }
+}
+
+
+
---
+++ Assets/HandGun.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGun : GunBase
+{
+    // 発射済みか否か
+    bool fired_ = false;
+    // トリガーを離したらfalse
+    public override void OffTrigger()
+    {
+        fired_ = false;
+    }
+
+    // 前ページから
+    public override void OnTrigger()
+    {
+        // 発射間隔以内なら早期リターン
+        if (shotTimer_ > 0) { return; }
+        // トリガー引きっぱなしなら早期リターン
+        if (fired_) { return; }
+        // 発射フラグON
+        fired_ = true;
+        // タイマーリセット
+        shotTimer_ = fireRate_;
+        // muzzleの正面にレイを飛ばす
+        Ray ray = new Ray(
+          muzzleTransform_.position,
+          muzzleTransform_.forward
+        );
+        RaycastHit raycastHit;
+        // 次ページへ
+
+        // 前ページから
+
+        // Itemレイヤーを無視する。LayerMask.GetMaskでその
+        // レイヤーのビットが1となっている値を取得した後、
+        // ~でビット反転を行なう。
+        int layerMask = ~LayerMask.GetMask(
+          new string[] { "Item" }
+        );
+        // レイの長さは雑に100mとする
+        float rayLength = 100;
+        // レイの終点はひとまず最大に
+        Vector3 endPoint = muzzleTransform_.position +
+          muzzleTransform_.forward * rayLength;
+
+        // 次ページへ
+
+        // 前ページから
+        // レイを飛ばす。
+        if (
+          Physics.Raycast(
+            ray,
+            out raycastHit,
+            rayLength,
+            layerMask
+          )
+        )
+        {
+            // 衝突地点にレイを短縮
+            endPoint = raycastHit.point;
+            // 対象がHelathコンポーネントを所持しているかを確認
+            Health healthComponent;
+            bool hasHealth =
+              raycastHit.collider.TryGetComponent(
+              out healthComponent
+            );
+            // 次ページへ
+            // 前ページから
+            // 持っていたらダメージを与える
+            if (hasHealth)
+            {
+                healthComponent.Damage(power_);
+            }
+        } // Physics.Raycastのifの終り
+          // 銃弾を生成・RayBulletコンポーネントの取得
+        GameObject bulletObject =
+          Instantiate(
+            bulletPrefab_.gameObject,
+            muzzleTransform_.position,
+            muzzleTransform_.rotation
+          );
+        RayBullet bullet =
+          bulletObject.GetComponent<RayBullet>();
+        // 次ページへ
+        // 次ページへ
+        // 描画するLineの始点と終点を設定
+        bullet.SetPositons(
+          muzzleTransform_.position,
+          endPoint
+        );
+    }
+
+
+
+}
